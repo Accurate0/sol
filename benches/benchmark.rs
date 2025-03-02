df92fa491af4f731d74f403ced2905989ac3d2f7
Anurag Singh <contact@anurag.sh>
2025-03-02 12:04:20 +0800

    add baseline and benchmark for typechecker

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,10 +1,10 @@
 use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
-use sol::{lexer::Lexer, parser::Parser};
+use sol::{lexer::Lexer, parser::Parser, typechecker::Typechecker};
 
 // FIXME: errors are causing the benchmark to basically increase in memory forever
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let input = include_str!("./input.rl");
+    let input = include_str!("./input.sol");
 
     c.bench_function("lexer", |b| {
         b.iter_batched(
@@ -26,6 +26,19 @@ fn criterion_benchmark(c: &mut Criterion) {
                 // parser returning result means error builds infinitely and ooms :)
                 let _ = parser.collect::<Vec<_>>();
             },
+            BatchSize::SmallInput,
+        )
+    });
+
+    c.bench_function("typechecker", |b| {
+        b.iter_batched(
+            || {
+                let lexer = Lexer::new(0, input);
+                Parser::new(lexer, input)
+                    .map(|s| s.unwrap())
+                    .collect::<Vec<_>>()
+            },
+            move |statements| Typechecker::default().check(&statements),
             BatchSize::SmallInput,
         )
     });
